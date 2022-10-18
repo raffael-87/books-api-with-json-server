3c4d026d1ab0bdf13ca3e62652ee9982d08bfe11
raffael-87 <101295119+raffael-87@users.noreply.github.com>
2022-10-18 20:54:30 +0200

    fix function

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const container = document.querySelector("#data-output");
-
 async function loadBooks() {
   const response = await fetch("db.json");
   const data = await response.json();
-
-  console.log(data);
   let template = "";
+
   for (let i = 0; i < data.books.length; i++) {
-    console.log(data.books[i].title);
     template += `
       			<tr>
       				<td>${data.books[i].title}</td>
@@ -18,35 +14,8 @@ async function loadBooks() {
       			</tr>
       		`;
   }
+
+  const container = document.querySelector("#data-output");
+  container.innerHTML = template;
 }
 loadBooks();
-
-container.innerHTML = template;
-
-// // const renderPosts = async (term) => {
-// //   console.log(term);
-// //   let uri = "http://localhost:3000/posts?_sort=likes&_order=desc";
-// //   if (term) {
-// //     uri += `&q=${term}`;
-// //   }
-
-// const res = await fetch("db.json");
-// const posts = await res.json();
-// console.log(posts);
-
-// let template = "";
-// posts.forEach((post) => {
-//   template += `
-//     			<tr>
-//     				<td>${book.title}</td>
-//     				<td>${book.subtitle}</td>
-//     				<td>${book.author}</td>
-//     				<td>${book.pages}</td>
-//     				<td>${book.isbn}</td>
-//     			</tr>
-//     		`;
-// });
-
-// container.innerHTML = template;
-
-// window.addEventListener("DOMContentLoaded", () => renderPosts());
